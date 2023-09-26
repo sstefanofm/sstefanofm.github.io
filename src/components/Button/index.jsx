a95efd8a8604b82995e703d0f0a16377475b6033
stf <sstefanofm@gmail.com>
2023-09-26 20:09:58 -0300

    Buttons use ThemeContext

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,14 @@
 import './Button.css'
 
+import { useContext } from 'react'
+import ThemeContext from '../../context/ThemeContext'
+
 const Button = ({ onclick, children }) => {
+  const { theme } = useContext(ThemeContext)
+
   return (
     <button
-      className='Button'
+      className={`Button Button--${theme}`}
       onClick={onclick}
     >
       {children}
