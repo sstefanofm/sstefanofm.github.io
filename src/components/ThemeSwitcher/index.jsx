2632f27b5215d9e6a8b75b9f450357f5757c40be
stf <sstefanofm@gmail.com>
2023-09-27 11:46:02 -0300

    fully dark/light themed

--- src/components/ThemeSwitcher/index.jsx
+++ src/components/ThemeSwitcher/index.jsx
@@ -3,14 +3,31 @@ import Theme from './theme'
 import Button from '../Button'
 import { Sun, Moon } from '../Icon'
 import ThemeContext from '../../context/ThemeContext'
+
 import { useContext } from 'react'
 
 const ThemeSwitcher = () => {
   const { theme, setTheme } = useContext(ThemeContext)
 
+  if (theme === Theme.LIGHT) {
+    document.querySelector('body').classList.add('Body--Light')
+  } else {
+    document.querySelector('body').classList.add('Body--Dark')
+  }
+
+  const changeBackground = () => {
+    document.querySelector('body').classList.toggle('Body--Dark')
+    document.querySelector('body').classList.toggle('Body--Light')
+  }
+
   if (theme === Theme.LIGHT)
     return (
-      <Button onclick={() => setTheme(Theme.DARK)}>
+      <Button
+        onclick={() => {
+          setTheme(Theme.DARK)
+          changeBackground()
+        }}
+      >
         <Moon
           width={30}
           height={30}
@@ -18,7 +35,12 @@ const ThemeSwitcher = () => {
       </Button>
     )
   return (
-    <Button onclick={() => setTheme(Theme.LIGHT)}>
+    <Button
+      onclick={() => {
+        setTheme(Theme.LIGHT)
+        changeBackground()
+      }}
+    >
       <Sun
         width={30}
         height={30}
