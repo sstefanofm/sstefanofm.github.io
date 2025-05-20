ba32a0b78ecc0276217b28cf3050818ba43a6c78
stf <sstefanofm@gmail.com>
2025-05-20 19:59:52 -0300

    ContentCard Cover and Content

--- src/components/ContentCard/index.jsx
+++ src/components/ContentCard/index.jsx
@@ -2,14 +2,33 @@ import './ContentCard.css'
 import working from '../../assets/working.gif'
 
 /* use this Card inside ContentBody */
-const ContentCard = () => {
-  return (<div className='ContentCard'>
+const ContentCard = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+ContentCard.Cover = () => {
+  return (
     <img
       className="WorkInProgress"
       src={working}
       alt="work in progress"
     />
-  </div>)
+  )
 }
+
+ContentCard.Content = () => {
+  return (
+    <div>
+      <p>This is text bro</p>
+    </div>
+  )
+}
+
+ContentCard.Cover.displayName = 'ContentCardCover'
+ContentCard.Content.displayName = 'ContentCardContent'
 
 export default ContentCard
