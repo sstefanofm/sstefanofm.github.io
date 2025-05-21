611d1f38e5845e010232c3a0fabd8c40609527a1
stf <sstefanofm@gmail.com>
2025-05-20 22:52:39 -0300

    MeWhenCoding initial text

--- src/components/MeWhenCoding/index.jsx
+++ src/components/MeWhenCoding/index.jsx
@@ -4,7 +4,14 @@ const MeWhenCoding = () => {
   return (
     <ContentCard>
       <ContentCard.Cover />
-      <ContentCard.Content />
+      <ContentCard.Content>
+        <p>Hi! JavaScript is the language that I&apos;ve spent most of my development career learning.</p>
+        <p>
+          Many things can be done with one-liners to look like a pro.
+          And OOP in JS is a joke, and I love non object oriented languages.
+        </p>
+        <p>This section of my webpage is to show cool things made with this awesome language.</p>
+      </ContentCard.Content>
     </ContentCard>
   )
 }
