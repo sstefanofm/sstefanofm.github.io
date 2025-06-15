0fb2d8de99c8cf1a8e408b95a58cd1abaa06dee3
stf <sstefanofm@gmail.com>
2025-06-15 19:19:32 -0300

    added FCM Microservice to projects

--- src/data/projects.js
+++ src/data/projects.js
@@ -12,12 +12,12 @@ export const projects = Object.freeze({
     'This portfolio. Where I publish my projects and some other things :D',
     'https://github.com/sstefanofm/sstefanofm.github.io/tree/dev/react',
   ),
-  DOTFILES: project(
-    'linux_logo.svg',
-    'dotfiles',
-    'md/dots',
-    'My dotfiles (in progress...)',
-    'https://github.com/sstefanofm/dots',
+  FCM_MICROSERVICE: project(
+    'firebase_logo.svg',
+    'FCM Microservice',
+    'md/fcm-microservice',
+    'A token management and push notifications microservice for Firebase Cloud Messaging made with Docker, Postgres and Java Spring',
+    'https://github.com/sstefanofm/fcm-microservice',
   ),
   JAVASCRIPT: project(
     'javascript_logo.svg',
@@ -25,7 +25,14 @@ export const projects = Object.freeze({
     'js',
     'Made with JavaScript',
     '/js'
-  )
+  ),
+  DOTFILES: project(
+    'linux_logo.svg',
+    'dotfiles',
+    'md/dots',
+    'My dotfiles (in progress...)',
+    'https://github.com/sstefanofm/dots',
+  ),
 })
 
 export const getProjects = () => Object.keys(projects)
